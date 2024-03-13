703d358436ccf5068d54fe5c0913fe1e28965115
MalmiNadee <malminadee324@gmail.com>
2024-03-13 20:53:25 +0530

    employee_manage

--- src/pages/employee_manage/ManagingDirector_view.js
+++ src/pages/employee_manage/ManagingDirector_view.js
@@ -1,7 +1,21 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import Header from '../../component/Header'
+import axios from 'axios'
 
 const ManagingDirector_view = () => {
+  const [state, setState] = useState({
+    empDetails: []
+})
+
+useEffect(() => {
+    axios.get("http://localhost:8000/empDetails/").then(res =>{
+        if(res.data){
+          setState({
+            empDetails:res.data
+          })
+        }
+      })
+    }, [state]);
   return (
     <>
     <div class="col">
@@ -10,9 +24,39 @@ const ManagingDirector_view = () => {
     <div class="container-fluid">
       <div class="row flex-nowrap">
         <div class="col py-3">
-        ManagingDirector_view employee manage
-          get details from inventory view
+        
         {/* table */}
+        <table class="table table-striped">
+                <thead>
+                    <tr>
+                    <th scope="col">Emp ID</th>
+                    <th scope="col">Name</th>
+                    <th scope="col">Date Of Birth</th>
+                    <th scope="col">Address</th>
+                    <th scope="col">Contact Number</th>
+                    <th scope="col">Position</th>
+                    <th scope="col">Department</th>
+                    <th scope="col">Joined Date</th>
+                    </tr>
+                </thead>
+                <tbody>
+                {state.empDetails.map((empDetails, index) => (
+                    <tr key={index}>
+                    <td>{empDetails.empID}</td>
+                    <td>{empDetails.name}</td>
+                    <td>{empDetails.dateOfBirth}</td>
+                    <td>{empDetails.address}</td>
+                    <td>{empDetails.contactNumber}</td>
+                    <td>{empDetails.position}</td>
+                    <td>{empDetails.department}</td>
+                    <td>{empDetails.joinedDate}</td>
+                    <td>
+                    
+                    </td>
+                    </tr>
+                ))}
+                </tbody>
+                </table>
         <button className='btn btn-primary mt-5' type='submit'>
             <a href="./report"  style={{textDecoration: 'none', color:'white'}}>Report</a>
       </button>
